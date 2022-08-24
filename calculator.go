13f151b7957dff75af59d11298e31e3d983cb5a4
rizkyardiansah <muhammadrizkyardiansah93@gmail.com>
2022-08-24 11:22:05 +0700

    initial commit

---
+++ calculator.go
@@ -0,0 +1,27 @@
+package go_calculator
+
+import "math"
+
+func Add(a int, b int) int {
+	return a + b
+}
+
+func Substract(a int, b int) int {
+	return a - b
+}
+
+func Multiply(a int, b int) int {
+	return a * b
+}
+
+func Divide(a int, b int) float64 {
+	return a/b
+}
+
+func Modulo(a int, b int) int {
+	return a % b
+}
+
+func Power(a float64, b float64) float64 {
+	return math.Pow(a, b)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rizkyardiansah/go-calculator
+
+go 1.19
